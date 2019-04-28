aba9baac48916168156a8eee846f74f498bc41a9
AliciaT <46272261+AliciaTransmuted@users.noreply.github.com>
2019-04-27 21:26:21 -0700

    Add files via upload
    April 27, 2019: Initial 0.1 release of DarkUnification-Look-and-Feel to github

---
+++ DarkUnification-Look-and-Feel/install.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+###
+### Dependancies: sed
+###
+
+###
+### Setup variables
+###
+
+user_name=$USER
+echo "Install DarkUnification KDE Plasma 5 Theme for ""${user_name}"
+
+backup_dir="~/.local/share/plasma/look-and-feel/DarkUnification/backups/"${user_name}"/"$(date '+%Y-%m-%d-%H-%M-%S')
+echo "Backup Directory: ""${backup_dir}"
+
+backup_files=0
+
+### 
+### Backup all config files this script will overwrite to a user named, date and time stamped directory
+### 
+
+if [ -e ~/.config/gtk-3.0/settings.ini ]; then
+   backup_files=$(( backup_files + 1 ))
+   temp_dir="${backup_dir}""/.config/gtk-3.0"
+   if [ ! -d "${temp_dir}" ]; then
+      mkdir -pv "${temp_dir}"
+   fi
+   cp -v ~/.config/gtk-3.0/settings.ini "${temp_dir}"
+fi
+
+if [ -e ~/.config/breezerc ]; then
+   backup_files=$(( backup_files + 1 ))
+   temp_dir="${backup_dir}""/.config"
+   if [ ! -d "${temp_dir}" ]; then
+      mkdir -pv "${temp_dir}"
+   fi
+   cp -v ~/.config/breezerc "${temp_dir}"
+fi
+
+if [ -e ~/.config/dolphinrc ]; then
+   backup_files=$(( backup_files + 1 ))
+   temp_dir="${backup_dir}""/.config"
+   if [ ! -d "${temp_dir}" ]; then
+      mkdir -pv "${temp_dir}"
+   fi
+   cp -v ~/.config/dolphinrc "${temp_dir}"
+fi
+
+if [ -e ~/.config/gtkrc ]; then
+   backup_files=$(( backup_files + 1 ))
+   temp_dir="${backup_dir}""/.config"
+   if [ ! -d "${temp_dir}" ]; then
+      mkdir -pv "${temp_dir}"
+   fi
+   cp -v ~/.config/gtkrc "${temp_dir}"
+fi
+
+if [ -e ~/.config/gtkrc-2.0 ]; then
+   backup_files=$(( backup_files + 1 ))
+   temp_dir="${backup_dir}""/.config"
+   if [ ! -d "${temp_dir}" ]; then
+      mkdir -pv "${temp_dir}"
+   fi
+   cp -v ~/.config/gtkrc-2.0 "${temp_dir}"
+fi
+
+if [ -e ~/.config/kwinrc ]; then
+   backup_files=$(( backup_files + 1 ))
+   temp_dir="${backup_dir}""/.config"
+   if [ ! -d "${temp_dir}" ]; then
+      mkdir -pv "${temp_dir}"
+   fi
+   cp -v ~/.config/kwinrc "${temp_dir}"
+fi
+
+if [ -e ~/.config/plasma-org.kde.plasma.desktop-appletsrc ]; then
+   backup_files=$(( backup_files + 1 ))
+   temp_dir="${backup_dir}""/.config"
+   if [ ! -d "${temp_dir}" ]; then
+      mkdir -pv "${temp_dir}"
+   fi
+   cp -v ~/.config/plasma-org.kde.plasma.desktop-appletsrc "${temp_dir}"
+fi
+
+echo "${backup_files}"" file(s) backed up to ""${backup_dir}"
+
+### 
+### Copy all data from DarkUnification Folder to new directories
+### 
+### Install the DarkUnification Theme
+### 
+
+cp -rv ~/DarkUnification-Look-and-Feel-master/DarkUnification-Look-and-Feel/config/* ~/.config
+cp -rv ~/DarkUnification-Look-and-Feel-master/DarkUnification-Look-and-Feel/themes/* ~/.themes
+cp -rv ~/DarkUnification-Look-and-Feel-master/DarkUnification-Look-and-Feel/color-schemes ~/.local/share/
+cp -rv ~/DarkUnification-Look-and-Feel-master/DarkUnification-Look-and-Feel/icons ~/.local/share/
+cp -rv ~/DarkUnification-Look-and-Feel-master/DarkUnification-Look-and-Feel/plasma ~/.local/share/
+
+sed -i -e s/xxUSERNAMExx/"${user_name}"/g ~/.local/share/plasma/look-and-feel/DarkUnification/contents/layouts/org.kde.plasma.desktop-layout.js
+
+tar -czvf ~/DarkUnification.tar.gz ~/.local/share/plasma/look-and-feel/DarkUnification
+
+kpackagetool5 -i ~/DarkUnification.tar.gz
+
+# copy config files after theme setup
+cp -rv ~/DarkUnification-Look-and-Feel-master/DarkUnification-Look-and-Feel/.config ~/
+cp -rv ~/DarkUnification-Look-and-Feel-master/DarkUnification-Look-and-Feel/.themes ~/
